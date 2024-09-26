0c8c18cc469183250005a4c667a9f64dc44bafd5
UrielSabugo <urielmaximiliano27@gmail.com>
2024-09-26 09:44:07 -0300

    nuevos metodos en correo y test

--- demo/src/main/java/com/example/Correo.java
+++ demo/src/main/java/com/example/Correo.java
@@ -1,14 +1,45 @@
 package com.example;
 import java.util.ArrayList;
-
+import java.util.List;
 public class Correo {
     
     private String asunto;
     private String contenido;
     private Contacto remitente;
+    private List<Contacto>para;
 
-    private ArrayList<Contacto>destinatario = new ArrayList();
+    /* CREACION DE CORREO CON SUS PROPIEDADES */
+public Correo(String asunto, String contenido, Contacto remitente){
+    this.asunto = asunto;
+    this.contenido = contenido;
+    this.remitente = remitente;
+    this.para = para;
+    }
+    
+    public String getAsunto(){
+        return asunto;
+    }
 
+    public String getContenido(){
+        return contenido;
+    }
+
+    public Contacto getRemitente(){
+        return remitente;
+    }
+    
+    
+    public List<Contacto> getPara(){
+        return para;
+    }
+    
+    /* UN USUARIO PUEDE ENVIAR CORREO A CONTACTOS Y AGREGAR VARIOS CONTACTOS EN EL PARA */
+    public void agregarDestinatario(Contacto destinatario){
+        if (para == null) {
+            para = new ArrayList<>();
+        }
+        para.add(destinatario);
+    }
 
 
 }
--- demo/src/test/java/com/example/ContactoTest.java
+++ demo/src/test/java/com/example/ContactoTest.java
@@ -1,8 +1,8 @@
 package com.example;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -21,13 +21,30 @@ public class ContactoTest
     public void crear_un_contacto_invalido_Test(){
 
         try {
-            new Contacto("Maria", "maria@ejemplo");
+            new Contacto("Uriel", "uriel@ejemplo");
                 fail("Se esperaba una IllegalArgumentException");
             }catch (IllegalArgumentException e) {
                 assertEquals("El email ingresado no es válido", e.getMessage());
         }
     
+    }
 
+    @Test
+    public void agregar_destinatario_test() {
+        Contacto remitente = new Contacto("Uriel Sabugo", "uriel@gmail.com");
+        Contacto destinatario1 = new Contacto("Nico Medula", "nico@gmail.com");
+        Contacto destinatario2 = new Contacto("Jose Fernandez", "josef@gmail.com");
+    
+        Correo correo = new Correo("probando", "capo.", remitente);
+    
+        correo.agregarDestinatario(destinatario1);
+        correo.agregarDestinatario(destinatario2);
+    
+        List<Contacto> destinatariosEsperados = new ArrayList<>();
+        destinatariosEsperados.add(destinatario1);
+        destinatariosEsperados.add(destinatario2);
+    
+        assertEquals(destinatariosEsperados, correo.getPara());
     }
     
 }
