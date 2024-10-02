29abf9de2590a0e3e29b8e7af3e8935023950d26
UrielSabugo <urielmaximiliano27@gmail.com>
2024-10-02 19:10:27 -0300

    metodo para borrar correos en IBandeja

--- src/main/java/com/example/IBandeja.java
+++ src/main/java/com/example/IBandeja.java
@@ -6,4 +6,7 @@ import java.util.function.Predicate;
 public interface IBandeja {
 
     public List<Correo> filtrarCorreos(Predicate<Correo> criterio);
+
+    public boolean borrarCorreo(Correo correo);
+    List<Correo> getCorreos();/* Metodo para obtener los correos */
 }
