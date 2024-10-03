4edd2829b7c26f6338919f27fcb28876c8205dad
Nico Medula <nicolasmedula@hotmail.com>
2024-10-02 21:08:21 -0300

    Borrar correos

--- src/main/java/com/example/IBandeja.java
+++ src/main/java/com/example/IBandeja.java
@@ -6,7 +6,5 @@ import java.util.function.Predicate;
 public interface IBandeja {
 
     public List<Correo> filtrarCorreos(Predicate<Correo> criterio);
-
-    public boolean borrarCorreo(Correo correo);
-    List<Correo> getCorreos();/* Metodo para obtener los correos */
+    
 }
